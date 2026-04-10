9ce3c1404a61254763913becdd958616ca3ff2c7
Davi-Freitass <davi_f_freitas@estudante.sesisenai.org.br>
2026-04-09 21:07:11 -0300

    add

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+let n1 = document.getElementById("1").value;
+let n2 = document.getElementById("2").value;
+let n3 = document.getElementById("3").value;
+let n4 = document.getElementById("4").value;
+let n5 = document.getElementById("5").value;
+let n6 = document.getElementById("6").value;
+let n7 = document.getElementById("7").value;
+let n8 = document.getElementById("8").value;
+let n9 = document.getElementById("9").value;
+let n0 = document.getElementById("0").value;
+let apaga = document.getElementById("apaga").value;
+let igual = document.getElementById("igual").value;
+let mais = document.getElementById("+").value;
+let menos = document.getElementById("-").value;
+let x = document.getElementById("x").value;
+let divide = document.getElementById("divide").value;
+});
